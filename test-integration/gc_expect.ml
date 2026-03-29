5a6ba2389b544e345b6672516bcb1b6c1592dabf
Alex Leighton <axlelonghorn@gmail.com>
2026-03-29 10:10:35 -0700

    test: add integration tests for auto-GC on open_kb
    Verify that open_kb triggers GC by using --gc-max-age 0d at init time:
    eligible Done todos are collected, non-terminal Open todos survive.

--- test-integration/gc_expect.ml
+++ test-integration/gc_expect.ml
@@ -61,6 +61,30 @@ let%expect_test "bs gc --json" =
     items_removed: 0
   |}]
 
+let%expect_test "auto-GC on open_kb collects eligible Done todo" =
+  Helper.with_git_root (fun dir ->
+    ignore (Helper.run_bs ~dir
+      ["init"; "-d"; dir; "-n"; "kb"; "--gc-max-age"; "0d"]);
+    ignore (Helper.run_bs ~dir ~stdin:"Body" ["add"; "todo"; "Task"]);
+    ignore (Helper.run_bs ~dir ["resolve"; "kb-0"]);
+    let result = Helper.run_bs ~dir ["list"] in
+    Helper.print_result ~dir result);
+  [%expect {|
+    [exit 0]
+  |}]
+
+let%expect_test "auto-GC on open_kb skips non-terminal Open todo" =
+  Helper.with_git_root (fun dir ->
+    ignore (Helper.run_bs ~dir
+      ["init"; "-d"; dir; "-n"; "kb"; "--gc-max-age"; "0d"]);
+    ignore (Helper.run_bs ~dir ~stdin:"Body" ["add"; "todo"; "Task"]);
+    let result = Helper.run_bs ~dir ["list"] in
+    Helper.print_result ~dir result);
+  [%expect {|
+    [exit 0]
+    kb-0    todo  open          Task
+  |}]
+
 let%expect_test "bs gc auto-rebuilds when db is missing" =
   Helper.with_git_root (fun dir ->
     Helper.init_kb dir;
